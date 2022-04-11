2f832f07986e8adbfb097805d403eb765f92e89a
muccidavide <davidemucci0804@gmail.com>
2022-04-11 13:43:02 +0200

    Complete snack #1

---
+++ jsnack_1/main.js
@@ -0,0 +1,79 @@
+/* 
+Crea un array composto da 10 automobili.
+Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+Infine stampa separatamente i 3 array.
+
+*/
+
+let cars = [
+    {
+    marca: "toyota",
+    modello: "yaris",
+    alimentazione: "benzina"
+},
+{
+    marca: "toyota",
+    modello: "Iq",
+    alimentazione: "diesel"
+},
+{
+    marca: "fiat",
+    modello: "500",
+    alimentazione: "benzina"
+},
+{
+    marca: "fiat",
+    modello: "600",
+    alimentazione: "diesel"
+},
+{
+    marca: "fiat",
+    modello: "punto",
+    alimentazione: "metano"
+},
+{
+    marca: "fiat",
+    modello: "500L",
+    alimentazione: "metano"
+},
+{
+    marca: "opel",
+    modello: "corsa",
+    alimentazione: "benzina"
+},
+{
+    marca: "opel",
+    modello: "meriva",
+    alimentazione: "metano"
+},
+{
+    marca: "seat",
+    modello: "ibiza",
+    alimentazione: "metano"
+},
+{
+    marca: "seat",
+    modello: "ibiza",
+    alimentazione: "benzina"
+},
+
+{
+    marca: "renault",
+    modello: "capture",
+    alimentazione: "metano"
+}
+
+]
+
+let benzina = cars.filter((element) => element.alimentazione === "benzina");
+console.log(benzina);
+
+let diesel = cars.filter((element) => element.alimentazione === "diesel");
+console.log(diesel);
+
+let metano = cars.filter((element) => element.alimentazione === "metano");
+console.log(metano);
+
+
+
